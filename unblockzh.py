7ddb233d90f18e6f93fdcb0145f45acf6f877b01
xiplus <huangxuanyuxiplus@gmail.com>
2021-10-27 22:36:29 +0800

    Fix credentials path

--- unblockzh.py
+++ unblockzh.py
@@ -32,12 +32,12 @@ class UnblockZh:
 
         creds = None
         if os.path.exists('token.json'):
-            creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+            creds = Credentials.from_authorized_user_file(BASE_DIR / 'token.json', SCOPES)
         if not creds or not creds.valid:
             if creds and creds.expired and creds.refresh_token:
                 creds.refresh(Request())
             else:
-                flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+                flow = InstalledAppFlow.from_client_secrets_file(BASE_DIR / 'credentials.json', SCOPES)
                 creds = flow.run_local_server(port=0)
             with open('token.json', 'w') as token:
                 token.write(creds.to_json())
